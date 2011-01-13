2bcdae9a45a765c272785c708e5140818ee98625
Josh Bassett <josh.bassett@gmail.com>
2011-01-13 21:41:56 +1100

    Use compass.

---
+++ config/compass.rb
@@ -0,0 +1,6 @@
+project_path = File.dirname(__FILE__)
+http_path    = '/'
+output_style = :expanded
+sass_dir     = 'content/stylesheets'
+css_dir      = 'output/stylesheets'
+sass_options = {:syntax => :scss}
