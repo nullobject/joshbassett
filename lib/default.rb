ca2e97359d7a3dafd5472deb8be445a2e5f91868
Josh Bassett <josh.bassett@gmail.com>
2011-01-30 17:12:37 +1100

    Override the long ordinal date format to be Australian.

--- lib/default.rb
+++ lib/default.rb
@@ -1,5 +1,8 @@
 require 'active_support/core_ext/date/conversions'
+require 'active_support/core_ext/integer/inflections'
 require 'nanoc-code-classifier'
 
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
+
+Date::DATE_FORMATS[:long_ordinal] = lambda {|date| date.strftime("#{date.day.ordinalize} %B %Y") }
