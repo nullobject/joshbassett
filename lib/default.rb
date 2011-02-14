72fa18b0a9816b5ca75e2350b3854c0119a79854
Josh Bassett <josh.bassett@gmail.com>
2011-02-14 18:30:04 +1100

    Group articles by year and month.

--- lib/default.rb
+++ lib/default.rb
@@ -6,3 +6,12 @@ include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 
 Date::DATE_FORMATS[:long_ordinal] = lambda {|date| date.strftime("#{date.day.ordinalize} %B %Y") }
+
+def articles_by_year_month
+  sorted_articles.group_by do |article|
+    created_at = article[:created_at]
+    [created_at.year, created_at.month]
+  end.sort_by do |(year, month), _|
+    [year, month]
+  end.reverse
+end
